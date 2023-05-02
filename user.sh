faca66918ce5f86fd625faa36ca32941d7ffc5f8
anusha <anushag2k16@gmail.com>
2023-05-02 16:06:46 +0530

    committing to roboshop-shell

---
+++ user.sh
@@ -0,0 +1,16 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+mkdir /app
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+cd /app
+unzip /tmp/user.zip
+npm install
+cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
+systemctl daemon-reload
+systemctl enable user
+systemctl start user
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org-shell -y
+mongo --h <ipadress> </app/schema/catalogue.js
+
