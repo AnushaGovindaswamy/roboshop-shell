a8a0f94310c772809748974c1ebd9b697c644ea2
anusha <anushag2k16@gmail.com>
2023-05-05 17:10:55 +0530

    committing to roboshop-shell

--- catalogue.sh
+++ catalogue.sh
@@ -1,16 +1,7 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-cd /app
-unzip /tmp/catalogue.zip
-npm install
-cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
-cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
-yum install mongodb-org-shell -y
-mongo --h <ipadress> </app/schema/catalogue.js
+script=$(realpath "$0")
+script_path=$(dirname "$script")
+source ${script_path}/common.sh
 
+component=catalogue
+schema_setup=mongo
+func_nodejs
